e7045dffd0323ce399951a396dfdaa2965753bed
diparfenov <dotza39@gmail.com>
2023-05-06 20:21:09 +0400

    1 day

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "materialize-css";
+
+import { useRoutes } from "./routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useAuth } from "./hooks/auth.hook";
 
 function App() {
+  //все параметры useAuth мы должны передвать всему приложннию
+  //для этого будем использовать контекст
+  const { token, userId, login, logout } = useAuth();
+  const routes = useRoutes(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">{routes}</div>
+    </Router>
   );
 }
 
